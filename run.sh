ca47858f4e0e3467995203a20151777f1224a0f0
masakifujiwara <fmasaki2020@gamil.com>
2023-08-08 17:44:16 +0900

    fix volume /dev/sensors

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-eval "docker container run --gpus all --rm -it --name box3_humble --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $HOME/share:/home/host_files --privileged tsukuba2023_humble:box3"
+eval "docker container run --network host --rm -it --name box3_humble -e DISPLAY=$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $HOME/docker_share:/home/host_files --privileged -v /dev/sensors:/dev/sensors tsukuba2023_humble:box3"
