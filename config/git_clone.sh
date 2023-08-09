dd7339fc74ac16fe6dfbf78ac2495c8c06983f14
masakifujiwara <fmasaki2020@gamil.com>
2023-08-09 19:20:33 +0900

    add git clone list

---
+++ config/git_clone.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+eval "git clone https://github.com/masakifujiwara1/adi_driver2.git"
+eval "git clone -b foxy-devel https://github.com/haruyama8940/icart_mini_driver"
+eval "git clone https://github.com/openspur/yp-spur"
+eval "git clone https://github.com/ros2/teleop_twist_joy"
