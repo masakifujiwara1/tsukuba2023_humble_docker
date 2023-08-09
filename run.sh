168196ca7435a826d60375b3524eafd858294628
masakifujiwara <fmasaki2020@gamil.com>
2023-08-09 15:35:40 +0900

    fix for using gpu

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-eval "docker container run --network host --rm -it --name box3_humble -e DISPLAY=$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $PWD/docker_share:/home/host_files --privileged -v /dev/sensors:/dev/sensors tsukuba2023_humble:box3"
+eval "docker container run --network host --gpus all --rm -it --name box3_humble -e DISPLAY=$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $PWD/docker_share:/home/host_files --privileged -v /dev/sensors:/dev/sensors tsukuba2023_humble:box3"
